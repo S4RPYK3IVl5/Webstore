bec3f7686e59d3488a406e313f3b21f4294dfedb
S4RPYK3IVl5 <S4RPYK3IVl5@users.noreply.github.com>
2019-06-27 14:21:34 +0400

    Commit2.1__Update_SecurityLogic

--- src/main/java/webstore/domain/User.java
+++ src/main/java/webstore/domain/User.java
@@ -2,28 +2,23 @@ package webstore.domain;
 
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
+import java.util.Collection;
 import java.util.Set;
 
 @Data
 @Table(name = "usr")
 @Entity
-public class User{
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private boolean active;
-
-    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
-    @Enumerated(EnumType.STRING)
-    private Set<Role> roles;
 
     @NotNull
     @Size(min = 6, max = 30)
@@ -32,6 +27,38 @@ public class User{
     @NotNull
     @Size(min = 6, max = 30)
     private String password;
+
+    private boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive();
+    }
 }
 
 
---
+++ src/main/java/webstore/service/UserService.java
@@ -0,0 +1,32 @@
+package webstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import webstore.domain.User;
+import webstore.repos.UserRepo;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    @Autowired
+    public UserService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByUsername(username);
+
+        if (user == null){
+            throw new UsernameNotFoundException("user not exist");
+        }
+
+        return user;
+    }
+}
